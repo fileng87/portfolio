717f16a14606a2770acf6fd6bb84f42cabbf73c4
lifeng87 <oscarcoll.930714@gmail.com>
2023-08-08 15:52:30 +0800

    guestbook layout

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -5,11 +5,9 @@ type Props = {};
 
 const GuestBook = (props: Props) => {
   return (
-    <div className="h-full w-full">
-      <div className="page-layout-center overflow-y-auto">
-        <div className="md:w-[60%] h-full md:pt-16 pt-24 md:px-6 px-4 backdrop-blur shadow-lg">
-          <Comment />
-        </div>
+    <div className="page-layout-center overflow-y-auto">
+      <div className="md:w-[60%] h-full w-full md:pt-16 pt-24 md:px-6 px-4 backdrop-blur md:shadow-lg">
+        <Comment />
       </div>
     </div>
   );
