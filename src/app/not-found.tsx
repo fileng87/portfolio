8741171b831141527f8cfcb621e63028257eac3f
lifeng87 <oscarcoll.930714@gmail.com>
2023-08-14 16:13:14 +0800

    not found page update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link';
-
 export default function NotFound() {
   return (
-    <div className="page-layout-center">
-      <span className="flex items-center gap-4">
+    <div className="page">
+      <span className="page-content flex min-h-screen items-center justify-center gap-4">
         <h2 className="border-e-[1px] border-neutral-500 pe-4 text-2xl">404</h2>
         <p>This page could not be found.</p>
       </span>
