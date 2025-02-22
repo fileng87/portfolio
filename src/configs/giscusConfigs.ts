f697e12a99516a468526e896edbea324cab398a2
lifeng87 <oscarcoll.930714@gmail.com>
2025-02-23 00:38:35 +0800

    v3 initial commit

--- src/configs/giscusConfigs.ts
+++
@@ -1,6 +0,0 @@
-export const giscusConfigs = {
-  repo: 'lifeng-87/lifeng-87' as `${string}/${string}`,
-  repoId: 'R_kgDOGZvnlw',
-  category: 'Announcements',
-  categoryId: 'DIC_kwDOGZvnl84CYa4L',
-};
