94ed6b0ba6103a20d05a6088bdc43d8a78c32fbc
ZyraX <oscarcoll.930714@gmail.com>
2026-04-29 14:30:35 +0800

    refactor: replace section wrapper with Hero component in Home for cleaner structure

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,16 +3,18 @@ import { cn } from "@/lib/utils";
 
 export default function Hero() {
   return (
-    <div
-      className={cn(
-        "absolute top-1/2 left-1/2",
-        "-translate-x-1/2 -translate-y-1/2",
-        "flex flex-wrap items-center justify-center",
-        "gap-x-24 gap-y-10",
-      )}
-    >
-      <Gravatar className="w-48 h-48" />
-      <h1 className="text-5xl font-bold text-center">Just code for fun.</h1>
-    </div>
+    <section className="h-screen w-screen relative" id="hero">
+      <div
+        className={cn(
+          "absolute top-1/2 left-1/2",
+          "-translate-x-1/2 -translate-y-1/2",
+          "flex flex-wrap items-center justify-center",
+          "gap-x-24 gap-y-10",
+        )}
+      >
+        <Gravatar className="w-48 h-48" />
+        <h1 className="text-5xl font-bold text-center">Just code for fun.</h1>
+      </div>
+    </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Hero from "./components/hero";
 export default function Home() {
   return (
     <main>
-      <section className="h-screen w-screen relative" id="hero">
-        <Hero />
-      </section>
+      <Hero />
     </main>
   );
 }
